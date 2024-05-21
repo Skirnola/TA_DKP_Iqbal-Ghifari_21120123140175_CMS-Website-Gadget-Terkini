70d23852b6477b017f1213c0bad5de3d04df0b8d
Skirnola <58127984+Skirnola@users.noreply.github.com>
2024-05-21 22:05:10 +0700

    Update login.php
    Nambahin style biar menu loginnya ditengah

--- admin/login.php
+++ admin/login.php
@@ -3,9 +3,9 @@ if (isset($_POST['login'])) {
    $username = $_POST['username'];
    $password = $_POST['password'];
    if ($username == "admin" && $password == "pass") {
-    session_start();
-    $_SESSION["user"] = "admin";
-    header("Location:index.php");
+       session_start();
+       $_SESSION["user"] = "admin";
+       header("Location:index.php");
    }
 }
 ?>
@@ -16,19 +16,39 @@ if (isset($_POST['login'])) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <style>
+        body, html {
+            height: 100%;
+            margin: 0;
+        }
+        .container {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+        }
+        .login-form {
+            width: 100%;
+            max-width: 400px;
+            padding: 20px;
+            background: #f8f9fa;
+            border-radius: 8px;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+        }
+    </style>
 </head>
 <body>
-    <div class="container mt-5" style="max-width:600px">
+    <div class="container">
         <div class="login-form">
             <form action="login.php" method="post">
                 <div class="form-field mb-4">
-                    <input class="form-control" type="text" name="username" id="" placeholder="Username">
+                    <input class="form-control" type="text" name="username" id="" placeholder="Username" required>
                 </div>
                 <div class="form-field mb-4">
-                    <input class="form-control" type="password" name="password" id="" placeholder="Password">
+                    <input class="form-control" type="password" name="password" id="" placeholder="Password" required>
                 </div>
                 <div class="form-field mb-4">
-                    <input class="btn btn-primary" type="submit" value="Login" name="login">
+                    <input class="btn btn-primary w-100" type="submit" value="Login" name="login">
                 </div>
             </form>
         </div>
