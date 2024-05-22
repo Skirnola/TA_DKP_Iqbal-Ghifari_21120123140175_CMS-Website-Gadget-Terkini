ea2e54f2fe995b44f19aa3ac1ba71878f7f21630
Skirnola <58127984+Skirnola@users.noreply.github.com>
2024-05-22 19:51:14 +0700

    Update delete.php

--- admin/delete.php
+++ admin/delete.php
@@ -1,16 +1,24 @@
 <?php
+function deletePost($conn, $id) {
+    $sqlDelete = "DELETE FROM posts WHERE id = $id";
+    if(mysqli_query($conn, $sqlDelete)){
+        return true;
+    } else {
+        return false;
+    }
+}
+
 $id = $_GET["id"];
 if($id){
-include("../connect.php");
-$sqlDelete = "DELETE FROM posts WHERE id = $id";
-if(mysqli_query($conn, $sqlDelete)){
-    session_start();
+    include("../connect.php");
+    if(deletePost($conn, $id)) {
+        session_start();
         $_SESSION["delete"] = "Postingan Berhasil Dihapus";
-    header("Location:index.php");
-}else{
-    die("Data Tidak Terhapus. Ada Kesalahan");
-}
-}else{
+        header("Location:index.php");
+    } else {
+        die("Data Tidak Terhapus. Ada Kesalahan");
+    }
+} else {
     echo "Postingan Tidak Ditemukan";
 }
 ?>
